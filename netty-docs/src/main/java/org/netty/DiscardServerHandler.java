3467634b28bf1577fbeb3227c44753308032934f
csh37 <csh37@woowahan.com>
2023-06-28 01:06:58 +0900

    feat : server that prints received msg

--- netty-docs/src/main/java/org/netty/DiscardServerHandler.java
+++ netty-docs/src/main/java/org/netty/DiscardServerHandler.java
@@ -13,9 +13,12 @@ public class DiscardServerHandler extends ChannelInboundHandlerAdapter { // (1)
   // This method is called with the received message, whenever new data is received from a client.
   @Override
   public void channelRead(ChannelHandlerContext ctx, Object msg) { // (2)
-    // Discard the received data silently.
     // ByteBuf is a reference-counted object which has to be released explicitly via the release() method.
-    ((ByteBuf) msg).release(); // (3)
+    ByteBuf in = (ByteBuf) msg;
+    while (in.isReadable()) { // (1)
+      System.out.print((char) in.readByte());
+      System.out.flush();
+    }
   }
 
   // This method is called with a Throwable when an exception was raised by Netty due to an I/O error or by a handler implementation due to the exception thrown while processing events.
