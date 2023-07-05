29e761d4717b2d098ef8e1b36396ddd61ca9d572
livenow14 <xowjd41@naver.com>
2023-07-05 20:07:07 +0900

    [webflux-docs] feat: 샘플코드 생성

---
+++ webflux-docs/src/main/java/com/netty/webfluxdocs/WebfluxDocsApplication.java
@@ -0,0 +1,13 @@
+package com.netty.webfluxdocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxDocsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WebfluxDocsApplication.class, args);
+  }
+
+}
---
+++ webflux-docs/src/test/java/com/netty/webfluxdocs/WebfluxDocsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netty.webfluxdocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxDocsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
